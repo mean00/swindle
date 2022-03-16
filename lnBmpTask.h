ebf9ebce49c4c2df3c37a1296f1d657e2bf018f9
mean <fixounet@free.fr>
2022-03-16 15:09:32 +0100

    reduce bmp stack size

--- lnBmpTask.h
+++ lnBmpTask.h
@@ -3,4 +3,4 @@
 #define TASK_BMP_SERIAL_PRIORITY    4
 #define TASK_BMP_SERIAL_STACK_SIZE  200 // in int32
 #define TASK_BMP_GDB_PRIORITY       5
-#define TASK_BMP_GDB_STACK_SIZE     600 // in int32
+#define TASK_BMP_GDB_STACK_SIZE     ((1680)/4) // in int32
