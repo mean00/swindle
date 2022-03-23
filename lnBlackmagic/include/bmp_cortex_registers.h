d2372eb726a61f76490c59c47a73e384760341c8
mean <fixounet@free.fr>
2022-03-23 08:16:08 +0100

    prepare register management

---
+++ lnBlackmagic/include/bmp_cortex_registers.h
@@ -0,0 +1,44 @@
+#pragma once
+
+class cortexRegs
+{
+public:
+    void storeRegistersToMemory(uint32_t adr)
+    {
+      for(int i=0;i<8;i++) // r4..r11
+        writeMem32(adr,i*4,_regs[4+i]);
+      adr+=8*4;
+      for(int i=0;i<4;i++) // r0..r3
+        writeMem32(adr,i*4,_regs[i]);
+      adr+=4*4;
+      writeMem32(adr,0*4,_regs[12]);
+      writeMem32(adr,1*4,_regs[14]);
+      writeMem32(adr,2*4,_regs[15]);
+      writeMem32(adr,3*4,_regs[16]); // psr
+    }
+    void loadRegistersFromMemory(uint32_t adr)
+    {
+      for(int i=0;i<8;i++) // r4..r11
+        _regs[4+i]=readMem32(adr,i*4);
+      adr+=8*4;
+      for(int i=0;i<4;i++) // r0..r3
+        _regs[i]=readMem32(adr,i*4);
+      adr+=4*4;
+      _regs[12]=readMem32(adr,0*4);
+      _regs[14]=readMem32(adr,1*4);
+      _regs[15]=readMem32(adr,2*4);
+      _regs[16]=readMem32(adr,3*4); // psr
+    }
+    void loadRegisters()
+    {
+        target_regs_read(cur_target,_regs);
+    }
+    void setRegisters()
+    {
+        target_regs_write(cur_target,_regs);
+    }
+
+protected:
+  uint32_t _regs[17]; //{16} is psr
+};
+//
---
+++ lnBlackmagic/include/bmp_gdb.h
@@ -0,0 +1,116 @@
+#pragma once
+/**
+
+*/
+class Gdb
+{
+public:
+  //
+
+  static bool startGatheringSymbol()
+  {
+    allSymbols.clear();
+    allSymbols.queryNextSymbol();
+    return true;
+  }
+
+  // Ask the current thread
+  static void Qc()
+  {
+    uint32_t pxCurrentTcb;
+    if(!allSymbols.readSymbolValue(spxCurrentTCB,pxCurrentTcb))
+    {
+      gdb_putpacketz("");
+      return;
+    }
+    uint32_t tid_adr=pxCurrentTcb;
+    Logger("Current TID ADR=%x\n",tid_adr);
+    uint32_t threadId=readMem32(tid_adr,O(OFFSET_TASK_NUM)); //68
+    Logger("Current TID =%x\n",threadId);
+
+    char tst[10+3];
+
+    if(!threadId) threadId=2;
+    sprintf(tst,"QC%x",threadId);
+    gdb_putpacket(tst,strlen(tst));
+    Logger(tst);
+
+  }
+  //
+  static void threadInfo(uint32_t  threadId)
+  {
+    findThread fnd(threadId);
+    fnd.run();
+    uint32_t tcb=fnd.tcb();
+    FreeRTOSSymbols sym=fnd.symbol();
+    if(!tcb) // assuming zero is not a valid address
+    {
+          gdb_putpacketz("E01");
+          return;
+    }
+    //
+    stringWrapper wrapper;
+
+    int maxLen=O(MAX_TASK_NAME_LEN);
+    uint32_t name=tcb+52;
+    char taskName[maxLen+1];
+    target_mem_read(cur_target,taskName,name,maxLen);
+    taskName[maxLen]=0;
+    wrapper.append(taskName);
+
+    const char *st;
+    switch(sym) // The queue it has been pulled from, gives the task state
+    {
+      case spxCurrentTCB:           st="R";break;
+      case sxSuspendedTaskList:     st="S";break;
+      case spxDelayedTaskList:      st="D";break;
+      case spxReadyTasksLists:      st="r";break;
+      default: st="?";break;
+    }
+    // Get task name now
+    wrapper.append("[");
+    wrapper.append(st);
+    wrapper.append("]");
+
+    wrapper.append("  TCB: 0x");
+    wrapper.appendHex32(tcb);
+
+
+    char *in=wrapper.string();;
+    int l=strlen(in);
+    char out[2*l+1];
+    hexify(out,in,l);
+    Logger(out);
+    gdb_putpacket(out, 2*l);
+  }
+  //
+  static bool decodeSymbol(int len, const char *packet)
+  {
+    allSymbols.decodeSymbol(len, packet);
+    return true;
+  }
+  static bool switchThread(uint32_t  threadId)
+  {
+    uint32_t currentTcb;
+    if(!allSymbols.readSymbolValue(spxCurrentTCB,currentTcb))
+    {
+      return false;
+    }
+    uint32_t currentThreadId=readMem32(currentTcb,O(OFFSET_TASK_NUM)); //68
+    if(currentThreadId==threadId)
+    {
+        Logger("Already on the right thread..\n");
+        gdb_putpacketz("OK");
+        return true;
+    }
+    // ok, first let's save on the current thread...
+
+    // restore other thread
+
+    gdb_putpacketz("OK");
+    return true;
+
+  }
+
+};
+
