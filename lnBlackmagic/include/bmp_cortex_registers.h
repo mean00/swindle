99e0647623586b0619241a6bf55a0d044185b657
mean <fixounet@free.fr>
2022-03-25 15:38:20 +0100

    preliminary M3/M4 support (incomplete)

--- lnBlackmagic/include/bmp_cortex_registers.h
+++ lnBlackmagic/include/bmp_cortex_registers.h
@@ -13,7 +13,26 @@
 class cortexRegs
 {
 public:
-    void storeRegistersButSpToMemory(uint32_t adr)
+        virtual uint32_t  storeRegistersButSpToMemory(uint32_t adr)=0;
+        virtual uint32_t  loadRegistersButSpFromMemory(uint32_t adr)=0;
+        virtual uint32_t  stackNeeded()=0;
+        virtual void      loadRegisters()=0;
+        virtual void      setRegisters()=0;
+        virtual uint32_t  read(int reg)=0;
+        virtual void      write(int reg,uint32_t val)=0;
+};
+/**
+
+*/
+class cortexRegsM3 : public cortexRegs
+{
+public:
+    uint32_t  stackNeeded()
+    {
+      return 4*(8+4+4);
+    }
+
+    uint32_t storeRegistersButSpToMemory(uint32_t adr)
     {
       for(int i=0;i<8;i++) // r4..r11
         writeMem32(adr,i*4,_regs[4+i]);
@@ -25,8 +44,10 @@ public:
       writeMem32(adr,1*4,_regs[14]);
       writeMem32(adr,2*4,_regs[15]);
       writeMem32(adr,3*4,_regs[16]); // psr
+      adr+=4*4;
+      return adr;
     }
-    void loadRegistersButSpFromMemory(uint32_t adr)
+    uint32_t loadRegistersButSpFromMemory(uint32_t adr)
     {
       for(int i=0;i<8;i++) // r4..r11
         _regs[4+i]=readMem32(adr,i*4);
@@ -38,6 +59,8 @@ public:
       _regs[14]=readMem32(adr,1*4);
       _regs[15]=readMem32(adr,2*4);
       _regs[16]=readMem32(adr,3*4); // psr
+      adr+=4*4;
+      return adr;
     }
     void loadRegisters()
     {
@@ -59,6 +82,107 @@ public:
      }
 
 protected:
-    uint32_t _regs[20]; // Arm mode no dp
+    uint32_t _regs[20]; // Arm mode no dp, dont try to shrink that !
 };
+
+/**
+
+*/
+class cortexRegsM4 : public cortexRegs
+{
+public:
+    uint32_t  stackNeeded()
+    {
+      return 4*(8+1+4+4);
+    }
+    uint32_t storeRegistersButSpToMemory(uint32_t adr)
+    {
+      for(int i=0;i<8;i++) // r4..r11
+          writeMem32(adr,i*4,_regs[4+i]);
+      adr+=8*4;
+      writeMem32(adr,8*4,_regs[14]);
+      adr+=4;
+      for(int i=0;i<4;i++) // r0..r3
+        writeMem32(adr,i*4,_regs[i]);
+      adr+=4*4;
+      writeMem32(adr,0*4,_regs[12]);
+      writeMem32(adr,1*4,_regs[14]);
+      writeMem32(adr,2*4,_regs[15]);
+      writeMem32(adr,3*4,_regs[16]); // psr
+      #warning add FPU regs here
+      adr+=4*4;
+      return adr;
+    }
+    uint32_t loadRegistersButSpFromMemory(uint32_t adr)
+    {
+      bool fpu=false;
+      for(int i=0;i<8;i++) // r4..r11
+          _regs[4+i]=readMem32(adr,i*4);
+      adr+=8*4;
+      _regs[14]=readMem32(adr,0);
+      adr+=1*4;
+      // are the FPU registers there ?
+      if(!(_regs[14] & 0x10))
+      {
+         // fpu !
+         fpu=true;
+         for(int i=0;i<16;i++)
+         {
+#warning add FPU regs here
+         }
+         adr+=16*4;
+      }
+      for(int i=0;i<4;i++) // r0..r3
+        _regs[i]=readMem32(adr,i*4);
+      adr+=4*4;
+      _regs[12]=readMem32(adr,0*4);
+      _regs[14]=readMem32(adr,1*4);
+      _regs[15]=readMem32(adr,2*4);
+      _regs[16]=readMem32(adr,3*4); // psr
+      adr+=4*4;
+
+      if(fpu)
+      {
+        adr+=18*4; // fpspr + reserver
+      }
+      return adr;
+    }
+    void loadRegisters()
+    {
+        target_regs_read(cur_target,_regs);
+        _regs[13]=_regs[18];
+    }
+    void setRegisters()
+    {
+        _regs[18]=_regs[13];
+        target_regs_write(cur_target,_regs);
+    }
+    uint32_t  read(int reg)
+    {
+        return _regs[reg];
+    }
+    void      write(int reg,uint32_t val)
+    {
+        _regs[reg]=val;
+     }
+
+protected:
+    uint32_t _regs[20+33]; // Arm regs + FPU
+};
+
+typedef struct CoreMatching
+{
+  const char *core;
+  uint8_t    coreId;
+};
+
+static const CoreMatching cores[]={
+    {"M0",0},
+    {"M0+",0},
+    {"M3",0},
+    {"M4",1},
+    {"M7",1},
+    {NULL,NULL},
+};
+extern cortexRegs *createCortexWrite(target *t);
 //
--- lnBlackmagic/src/bmp_gdb_cmd.cpp
+++ lnBlackmagic/src/bmp_gdb_cmd.cpp
@@ -20,58 +20,58 @@
 #endif
 
 
-  bool Gdb::startGatheringSymbol()
-  {
-    allSymbols.clear();
-    allSymbols.queryNextSymbol();
-    return true;
-  }
+bool Gdb::startGatheringSymbol()
+{
+  allSymbols.clear();
+  allSymbols.queryNextSymbol();
+  return true;
+}
 
-  uint32_t Gdb::getCurrentThreadId()
+uint32_t Gdb::getCurrentThreadId()
+{
+  uint32_t pxCurrentTcb;
+  if(!allSymbols.readSymbolValue(spxCurrentTCB,pxCurrentTcb))
   {
-    uint32_t pxCurrentTcb;
-    if(!allSymbols.readSymbolValue(spxCurrentTCB,pxCurrentTcb))
-    {
-      return 0;
-    }
-    uint32_t threadId=readMem32(pxCurrentTcb,O(OFFSET_TASK_NUM)); //68
-    return threadId;
+    return 0;
   }
-  // Ask the current thread
-  void Gdb::Qc()
-  {
-    uint32_t threadId=getCurrentThreadId();
-    char tst[10+3];
+  uint32_t threadId=readMem32(pxCurrentTcb,O(OFFSET_TASK_NUM)); //68
+  return threadId;
+}
+// Ask the current thread
+void Gdb::Qc()
+{
+  uint32_t threadId=getCurrentThreadId();
+  char tst[10+3];
 
-    //if(!threadId) /threadId=2;
-    sprintf(tst,"QC%x",threadId);
-    gdb_putpacket(tst,strlen(tst));
-    Logger(tst);
-    GDB_LOGGER(tst);
+  //if(!threadId) /threadId=2;
+  sprintf(tst,"QC%x",threadId);
+  gdb_putpacket(tst,strlen(tst));
+  Logger(tst);
+  GDB_LOGGER(tst);
+}
+//
+void Gdb::threadInfo(uint32_t  threadId)
+{
+  lnThreadInfo *info=threadCache->searchForTid(threadId);
+  if(!info)
+  {
+    gdb_putpacketz("E01");
+    return;
+  }
+  uint32_t tcb=info->tcb;
+  if(!tcb) // assuming zero is not a valid address
+  {
+        gdb_putpacketz("E01");
+        return;
   }
   //
-  void Gdb::threadInfo(uint32_t  threadId)
-  {
-    lnThreadInfo *info=threadCache->searchForTid(threadId);
-    if(!info)
-    {
-      gdb_putpacketz("E01");
-      return;
-    }
-    uint32_t tcb=info->tcb;
-    if(!tcb) // assuming zero is not a valid address
-    {
-          gdb_putpacketz("E01");
-          return;
-    }
-    //
-    stringWrapper wrapper;
-    int maxLen=O(MAX_TASK_NAME_LEN);
-    uint32_t name=tcb+O(OFFSET_TASK_NAME);
-    char taskName[maxLen+1];
-    target_mem_read(cur_target,taskName,name,maxLen);
-    taskName[maxLen]=0;
-    wrapper.append(taskName);
+  stringWrapper wrapper;
+  int maxLen=O(MAX_TASK_NAME_LEN);
+  uint32_t name=tcb+O(OFFSET_TASK_NAME);
+  char taskName[maxLen+1];
+  target_mem_read(cur_target,taskName,name,maxLen);
+  taskName[maxLen]=0;
+  wrapper.append(taskName);
 
 #if 0
     const char *st;
@@ -139,13 +139,13 @@
     // Save current thread
     //-----------------------
 
-    cortexRegs *regs=new cortexRegs;
+    cortexRegs *regs=createCortexWrite(cur_target);
     regs->loadRegisters();
     uint32_t sp=regs->read(13);
 
     GDB_LOGGER("Current Thread ID=%d, current TCB=%x sp=%x\n",currentThreadId,currentTcb,sp);
     GDB_LOGGER("PC=%x, SP=%x\n",regs->read(14),sp);
-    sp-=4*16;
+    sp-=regs->stackNeeded();
     regs->storeRegistersButSpToMemory(sp);
     writeMem32(currentTcb,0,sp); // store sp on the TCB for current stack
     //
@@ -153,8 +153,7 @@
     //----------------------------
     sp=readMem32(tcb,0); // top of stack
     GDB_LOGGER("New Thread ID=%d, thread TCB=%x sp=%x\n",threadId,tcb,sp);
-    regs->loadRegistersButSpFromMemory(sp);
-    sp+=4*16;
+    sp=regs->loadRegistersButSpFromMemory(sp);
     GDB_LOGGER("PC=%x, SP=%x\n",regs->read(14),sp);
     regs->write(13,sp);   // update sp
     regs->setRegisters(); // set actual registers from regs
@@ -186,5 +185,26 @@ bool Gdb::isThreadAlive(uint32_t  threadId)
     return false;
   }
   return true;
+}
+
+cortexRegs *createCortexWrite(target *t)
+{
+  const CoreMatching *c=cores;
+  const char *name=t->core;
+  while(c->core)
+  {
+    if(!strcmp(name,c->core))
+    {
+      switch(c->coreId)
+      {
+          default: Logger("Cannot identify core\n");
+          case 0: return new cortexRegsM3;break;
+          case 1: return new cortexRegsM4;break;
+      }
+    }
+    c++;
+  }
+  Logger("Unknown core %d\n",name);
+  return new cortexRegsM3;
 }
 // EOF
