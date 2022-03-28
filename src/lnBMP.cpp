f8ce8d0f34c1b634e833bdcf480ea507c7d1c065
mean <fixounet@free.fr>
2022-03-28 11:01:12 +0200

    blink both PA8 and PC13

--- src/lnBMP.cpp
+++ src/lnBMP.cpp
@@ -1,6 +1,7 @@
 #include "lnArduino.h"
 
 #define LED LN_SYSTEM_LED
+#define LED2 PA8
 
 extern "C" void user_init();
 
@@ -9,25 +10,17 @@ extern "C" void user_init();
 void setup()
 {
     lnPinMode(LED, lnOUTPUT);
+    lnPinMode(LED2, lnOUTPUT);
 }
 void loop()
 {
-  #if 0
-#define ZZZ PB3
-    lnPinMode(ZZZ, lnOUTPUT);
-    while (1)
-    {
-        Logger("*\n");
-        delay(1000);
-        lnDigitalToggle(ZZZ);
-    }
-#endif
     Logger("Starting lnBMP Test\n");
     user_init();
     while (1)
     {
-        //Logger("*\n");
+        // Logger("*\n");
         delay(1000);
         lnDigitalToggle(LED);
+        lnDigitalToggle(LED2);
     }
 }
