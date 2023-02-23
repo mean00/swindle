883ff9f1342de3e89567ef8b9e9032ed532578dd
mean <fixounet@free.fr>
2023-02-23 15:33:47 +0100

    reduce bootloader to 8K

--- mcuSelect.cmake
+++ mcuSelect.cmake
@@ -11,7 +11,7 @@ IF(1) # ARM
             #      SET(LN_MCU_SPEED  72000000    CACHE INTERNAL "") #=> ok
       SET(LN_SPEC         "picolibc"   CACHE INTERNAL "") # if not set we use nano
       SET(LN_ENABLE_USBD True CACHE INTERNAL "")
-      SET(LN_BOOTLOADER_SIZE 16 CACHE INTERNAL "")
+      SET(LN_BOOTLOADER_SIZE 8     CACHE INTERNAL "")
   ELSE()
       SET(LN_ARCH            "ARM" CACHE INTERNAL "")
       SET(LN_MCU             "M3"  CACHE INTERNAL "")
@@ -25,7 +25,7 @@ IF(1) # ARM
         SET(LN_MCU_STATIC_RAM  4     CACHE INTERNAL "")
         SET(LN_MCU_FLASH_SIZE  128   CACHE INTERNAL "")
         SET(LN_MCU_EEPROM_SIZE 4     CACHE INTERNAL "")
-        SET(LN_BOOTLOADER_SIZE 16 CACHE INTERNAL "")
+        SET(LN_BOOTLOADER_SIZE 8     CACHE INTERNAL "")
         SET(LN_ENABLE_USBD True CACHE INTERNAL "")
       ENDIF()
       SET(LN_SPEC         "picolibc"   CACHE INTERNAL "") # if not set we use nano
