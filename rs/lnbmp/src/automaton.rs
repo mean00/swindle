4ed4c3ef0282928bf862a18370f4f689c93fa0a6
mean <fixounet@free.fr>
2023-02-25 17:50:51 +0100

    basic parser

---
+++ rs/lnbmp/src/automaton.rs
@@ -0,0 +1,127 @@
+#![no_std]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+//
+//
+#[derive(PartialEq)]
+enum PARSER_AUTOMATOMON
+{
+    Init,
+    Idle,
+    Body,
+    Escape,
+    End1,
+    End2,  
+    Done,  
+}
+#[derive(PartialEq)]
+pub enum RESULT_AUTOMATON
+{
+    Continue,
+    Ready,
+    Error,
+}
+const INPUT_BUFFER_SIZE : usize = 256;
+
+const CHAR_RESET_04 : u8 = 0x04;
+const CHAR_START : u8 = b'$';
+const CHAR_END : u8 = b'#';
+const CHAR_ESCAPE : u8 = b'}';
+//
+pub struct input_stream
+{    
+    automaton       : PARSER_AUTOMATOMON,
+    input_buffer    : [u8;INPUT_BUFFER_SIZE],
+    indx            : usize,
+}
+impl input_stream
+{
+    pub fn new() -> Self
+    {
+        input_stream
+        {
+            
+            automaton       : PARSER_AUTOMATOMON::Idle,
+            input_buffer    : [0;INPUT_BUFFER_SIZE],
+            indx            : 0,
+        }
+    }
+    pub fn get_result(&mut self) -> &[u8]
+    {
+        self.automaton = PARSER_AUTOMATOMON::Idle;        
+        &self.input_buffer[0..self.indx]
+    }
+    pub fn parse( &mut self, data : &[u8]) -> (usize, RESULT_AUTOMATON)
+    {
+        let mut dex =0;
+        let mut sz = data.len();
+        let mut consumed = 0;
+        while sz>0
+        {
+            
+            let c : u8 = data[dex];
+            consumed+=1;
+            sz-=1;
+            dex+=1;
+            self.automaton  = match self.automaton 
+            {
+                PARSER_AUTOMATOMON::Init => 
+                                                match c
+                                                {
+                                                    CHAR_RESET_04 => PARSER_AUTOMATOMON::Idle,
+                                                    _    => PARSER_AUTOMATOMON::Init,
+                                                }
+                                            ,
+                PARSER_AUTOMATOMON::Idle => 
+                                                match c
+                                                {
+                                                    CHAR_START /*'$'*/ => {self.indx = 0;PARSER_AUTOMATOMON::Body}, 
+                                                    CHAR_RESET_04 => PARSER_AUTOMATOMON::Idle,
+                                                    _    => PARSER_AUTOMATOMON::Idle,
+                                                }
+                                            ,                  
+                PARSER_AUTOMATOMON::Body => 
+                                            match c
+                                            {
+                                                CHAR_END /*'#'*/  => PARSER_AUTOMATOMON::End1, 
+                                                CHAR_ESCAPE /*'}'*/  => PARSER_AUTOMATOMON::Escape, 
+                                                CHAR_RESET_04 => PARSER_AUTOMATOMON::Idle,
+                                                _    => {
+                                                            self.input_buffer[self.indx]=c;
+                                                            self.indx+=1;
+                                                            PARSER_AUTOMATOMON::Body
+                                                        },
+                                            }
+                                        ,                                                                                              
+                PARSER_AUTOMATOMON::Escape => 
+                                        {
+                                            self.input_buffer[self.indx]=c^20;
+                                            self.indx+=1;
+                                            PARSER_AUTOMATOMON::Body
+                                        },
+                PARSER_AUTOMATOMON::End1 => 
+                                        {
+                                            PARSER_AUTOMATOMON::End2
+                                        },                                        
+                PARSER_AUTOMATOMON::End2 => 
+                                        {
+                                            PARSER_AUTOMATOMON::Done
+                                        },  
+                PARSER_AUTOMATOMON::Done => panic!("automaton"), //; PARSER_AUTOMATOMON::Done},
+            };
+            if self.automaton ==  PARSER_AUTOMATOMON::Done
+            {
+                return (consumed, RESULT_AUTOMATON::Ready);
+            }
+        }
+        return (consumed, RESULT_AUTOMATON::Continue);
+    }
+
+}
+//
+
+// EOF
--- rs/lnbmp/src/lib.rs
+++ rs/lnbmp/src/lib.rs
@@ -6,114 +6,13 @@
 #![allow(unused_imports)]
 
 mod util;
-mod linear_ring;
-
+mod automaton;
+use crate::automaton::input_stream;
 extern crate alloc;
+
 extern crate std;
-
 use std::print;
-//
-//
-#[derive(PartialEq)]
-enum PARSER_AUTOMATOMON
-{
-    Init,
-    Idle,
-    Body,
-    Escape,
-    End1,
-    End2,  
-    Done,  
-}
-enum RESULT_AUTOMATON
-{
-    Continue,
-    Ready,
-    Error,
-}
-
-//
-struct input_stream
-{
-    ring        : linear_ring::linear_ring<512>,
-    automaton   : PARSER_AUTOMATOMON,
-}
-impl input_stream
-{
-    pub fn new() -> Self
-    {
-        input_stream
-        {
-            ring        : linear_ring::linear_ring::new(),
-            automaton   : PARSER_AUTOMATOMON::Idle,
-        }
-    }
-    pub fn parse( &mut self, data : &[u8]) -> (usize, RESULT_AUTOMATON)
-    {
-        let mut dex =0;
-        let mut sz = data.len();
-        let mut consumed = 0;
-        while sz>0
-        {
-            let c : u8 = data[dex];
-            consumed+=1;
-            sz-=1;
-            dex+=1;
-            self.automaton  = match self.automaton 
-            {
-                PARSER_AUTOMATOMON::Init => 
-                                                match c
-                                                {
-                                                    0x04 => PARSER_AUTOMATOMON::Idle,
-                                                    _    => PARSER_AUTOMATOMON::Init,
-                                                }
-                                            ,
-                PARSER_AUTOMATOMON::Idle => 
-                                                match c
-                                                {
-                                                    0x24 /*'$'*/ => PARSER_AUTOMATOMON::Body, 
-                                                    0x04 => PARSER_AUTOMATOMON::Idle,
-                                                    _    => PARSER_AUTOMATOMON::Idle,
-                                                }
-                                            ,                  
-                PARSER_AUTOMATOMON::Body => 
-                                            match c
-                                            {
-                                                0x23 /*'#'*/  => PARSER_AUTOMATOMON::End1, 
-                                                0x7d /*'}'*/  => PARSER_AUTOMATOMON::Escape, 
-                                                0x04 => PARSER_AUTOMATOMON::Idle,
-                                                _    => {
-                                                            
-                                                            PARSER_AUTOMATOMON::Body
-                                                        },
-                                            }
-                                        ,                                                                                              
-                PARSER_AUTOMATOMON::Escape => 
-                                        {
-                                            PARSER_AUTOMATOMON::Body
-                                        },
-                PARSER_AUTOMATOMON::End1 => 
-                                        {
-                                            PARSER_AUTOMATOMON::End2
-                                        },                                        
-                PARSER_AUTOMATOMON::End2 => 
-                                        {
-                                            PARSER_AUTOMATOMON::Done
-                                        },  
-                PARSER_AUTOMATOMON::Done => {panic!("automaton"); PARSER_AUTOMATOMON::Done},
-            };
-            if self.automaton ==  PARSER_AUTOMATOMON::Done
-            {
-                print!("!!!!!!!!!!!!!!\n");
-                self.automaton = PARSER_AUTOMATOMON::Idle;
-                return (consumed, RESULT_AUTOMATON::Ready);
-            }
-
-        }
-        return (consumed, RESULT_AUTOMATON::Continue);
-    }
-
-}
+use alloc::vec::Vec;
 //
 #[no_mangle]
 
@@ -149,7 +48,28 @@ extern "C" fn rngdbstub_run(l : usize, d : *const cty::c_uchar )
     let data_as_slice : &[u8] = core::slice::from_raw_parts(d,l);    
     match autoauto
     {
-        Some(ref mut x) => x.parse(data_as_slice),
+        Some(ref mut x) => 
+                    {
+                        let _consumed : usize;
+                        let state : crate::automaton::RESULT_AUTOMATON;
+                        let empty : &str = "";
+                        (_consumed, state) =  x.parse(data_as_slice);
+                        if state == crate::automaton::RESULT_AUTOMATON::Ready
+                        {
+                            // ok we have a full string...
+                            let s = x.get_result();
+                            let flat_string  = match core::str::from_utf8(s)
+                            {
+                                Ok(x)       => x,
+                                Err(y) => empty,
+                            };
+                            let tokns : Vec <&str>= flat_string.split_whitespace().collect();
+                            print!("{:?}\n",flat_string);                        
+                            print!("{:?}\n",tokns);    
+                        
+                        }
+
+                    },
         None => panic!("noauto"),
     };
     }
