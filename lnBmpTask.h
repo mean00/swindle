0800c36f9301717b976e4e7e380dd49b1483b9f0
mean <fixounet@free.fr>
2023-02-28 10:25:21 +0100

    increase stack size

--- lnBmpTask.h
+++ lnBmpTask.h
@@ -4,4 +4,4 @@
 #define TASK_BMP_SERIAL_STACK_SIZE  200 // in int32
 #define TASK_BMP_GDB_PRIORITY       5
 //#define TASK_BMP_GDB_STACK_SIZE     ((1680)/4) // in int32
-#define TASK_BMP_GDB_STACK_SIZE     ((2000)/4) // in int32
+#define TASK_BMP_GDB_STACK_SIZE     ((4000)/4) // in int32
