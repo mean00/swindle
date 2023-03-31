607b2ab698e61f710c1a201b72735831bd705805
mean <fixounet@free.fr>
2023-03-31 15:19:14 +0200

    ch32: increase clock speed to 96M

--- mcuSelect.cmake
+++ mcuSelect.cmake
@@ -2,14 +2,16 @@
 IF(NOT DEFINED LN_ARCH)
   IF(USE_CH32V3x) # RISCV
     # try without crystal...
-    #  SET(LN_USE_INTERNAL_CLOCK True        CACHE INTERNAL "")
-    SET(LN_MCU_SPEED          48000000    CACHE INTERNAL "") # 96 Mhz
+    #SET(LN_USE_INTERNAL_CLOCK True        CACHE INTERNAL "")
+    #SET(LN_MCU_SPEED          48000000    CACHE INTERNAL "") # 96 Mhz
+
+    SET(LN_MCU_SPEED          96000000    CACHE INTERNAL "") # 96 Mhz
 
     SET(LN_ARCH "RISCV"                   CACHE INTERNAL "")
     SET(LN_MCU  "CH32V3x"                 CACHE INTERNAL "")
     SET(LN_MCU_RAM_SIZE       64          CACHE INTERNAL "")
     SET(LN_MCU_FLASH_SIZE     256         CACHE INTERNAL "")
-    SET(LN_MCU_STATIC_RAM     6           CACHE INTERNAL "")
+    SET(LN_MCU_STATIC_RAM     8           CACHE INTERNAL "")
     SET(LN_SPEC               "picolibc"  CACHE INTERNAL "") # if not set we use nano
     SET(LN_BOOTLOADER_SIZE    0     CACHE INTERNAL "")
     #SET(LN_SPEC         "picolibc"   CACHE INTERNAL "") # if not set we use nano
