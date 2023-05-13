5de248695b61bcd23d7b4f8141d74811ea8d2cec
mean <fixounet@free.fr>
2023-05-13 10:09:03 +0200

    make supported boards "easily" configurable

--- lnBlackmagic/src/bmp_disabledBoard.cpp
+++ lnBlackmagic/src/bmp_disabledBoard.cpp
@@ -7,41 +7,16 @@ extern "C"
 #include "adiv5.h"
 #define BOARD_STUB(x) bool x(adiv5_access_port_s *apb) {return false;}
 
-//grep undefined /tmp/x | sort | uniq | sed 's/.*reference to .//g' | sed 's/.$/);/g' | sed 's/^/BOARD_STUB(/g' | sed 's/ //g' | uniq
-
-BOARD_STUB(imxrt_probe);
-BOARD_STUB(lpc55xx_probe);
-BOARD_STUB(lpc55_dp_prepare);
-BOARD_STUB(lpc55_dmap_prepare);
-BOARD_STUB(lpc55_dmap_probe);
-BOARD_STUB(lpc40xx_probe);
-
-BOARD_STUB(efm32_aap_probe);
-BOARD_STUB(efm32_probe);
-BOARD_STUB(ke04_probe);
-BOARD_STUB(kinetis_mdm_probe);
-BOARD_STUB(kinetis_probe);
-BOARD_STUB(lpc15xx_probe);
-BOARD_STUB(lpc17xx_probe);
-BOARD_STUB(lpc43xx_probe);
-BOARD_STUB(lpc546xx_probe);
-BOARD_STUB(msp432_probe);
-BOARD_STUB(nrf51_mdm_probe);
-BOARD_STUB(nrf51_probe);
-//BOARD_STUB(rp_probe);
-//BOARD_STUB(rp_rescue_probe);
-BOARD_STUB(sam3x_probe);
-BOARD_STUB(sam4l_probe);
-BOARD_STUB(samd_probe);
-BOARD_STUB(samx5x_probe);
-BOARD_STUB(stm32h7_probe);
-
-BOARD_STUB(samx7x_probe);
-BOARD_STUB(renesas_probe);
+#include "ln_board_stubs.h"
 
 bool cortexa_probe(adiv5_access_port_s *apb, uint32_t debug_base)
 {
   return false;
 }
 
+}
+
+const char *list_enabled_boards()
+{
+  return LN_BOARDS_ENABLED;
 }
