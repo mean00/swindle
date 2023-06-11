52b0ac670c6a5aa30025d0f0693b2f519a908433
mean <fixounet@free.fr>
2023-06-11 08:06:49 +0200

    GPIOS: when NRESET is not asserted, keep it floating

--- lnBlackmagic/src/bmp_gpio.cpp
+++ lnBlackmagic/src/bmp_gpio.cpp
@@ -1,54 +1,52 @@
 
 #include "lnArduino.h"
-#include "lnGPIO.h"
 #include "lnBMP_pinout.h"
+#include "lnGPIO.h"
 
 extern "C" void bmp_gpio_write(int pin, int value);
-extern "C" int  bmp_gpio_read(int pin);
+extern "C" int bmp_gpio_read(int pin);
 extern "C" void bmp_gpio_drive_state(int pin, int driven);
 
 extern "C" void platform_nrst_set_val(bool assert);
 extern "C" void platform_target_clk_output_enable(bool enable);
 extern "C" bool platform_nrst_get_val(void);
 
-
 /**
-*/
+ */
 void bmp_gpio_write(int pin, int value)
 {
-  xAssert(0);
+    xAssert(0);
 }
 /**
-*/
-int  bmp_gpio_read(int pin)
+ */
+int bmp_gpio_read(int pin)
 {
-  xAssert(0);
-  return 0;
+    xAssert(0);
+    return 0;
 }
 
 void platform_nrst_set_val(bool assert)
 {
-  lnPin pin=_mapping[TRESET_PIN];
-  if(assert)
-  {
-    lnPinMode(pin,lnOUTPUT);
-    lnDigitalWrite(pin, 0);
-  }else
-  {
-    lnPinMode(pin,lnINPUT_PULLDOWN);
-  }
+    lnPin pin = _mapping[TRESET_PIN];
+    if (assert)
+    {
+        lnPinMode(pin, lnOUTPUT);
+        lnDigitalWrite(pin, 0);
+    }
+    else
+    {
+        lnPinMode(pin, lnINPUT_FLOATING);
+    }
 }
 bool platform_nrst_get_val(void)
 {
 
-  return lnDigitalRead(_mapping[TRESET_PIN]);
+    return lnDigitalRead(_mapping[TRESET_PIN]);
 }
 
 void platform_target_clk_output_enable(bool enable)
 {
-  (void)enable;
+    (void)enable;
 }
-
-
 
 // EOF
