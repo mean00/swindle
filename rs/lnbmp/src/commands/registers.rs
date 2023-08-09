524f7dc59bdfbec1ac3fd02ab1b94e014759390e
mean <fixounet@free.fr>
2023-08-09 07:47:58 +0200

    register: fix le vs be

--- rs/lnbmp/src/commands/registers.rs
+++ rs/lnbmp/src/commands/registers.rs
@@ -1,16 +1,15 @@
-
 use alloc::vec;
 use alloc::vec::Vec;
 
+use super::{exec_one, CommandTree};
 use crate::encoder::encoder;
-use super::{CommandTree,exec_one};
 use crate::packet_symbols::INPUT_BUFFER_SIZE;
 
-use super::mon::{_swdp_scan};
+use super::mon::_swdp_scan;
 use crate::bmp::bmp_attached;
 use crate::bmp::bmp_get_mapping;
+use crate::bmp::mapping::{FLASH, RAM};
 use crate::bmp::MemoryBlock;
-use crate::bmp::mapping::{ FLASH,RAM};
 
 use crate::parsing_util::ascii_string_to_u32;
 
@@ -24,29 +23,59 @@ crate::setup_log!(false);
 // Pf=40000008
 //
 
-fn pp_prefix( command : &str) -> Option<(u32,u32)>
-{       
-    let args : Vec <&str>= command.split('=').collect();
-    if args.len()!=2
-    {
+fn pp_prefix(command: &str) -> Option<(u32, u32)> {
+    let args: Vec<&str> = command.split('=').collect();
+    if args.len() != 2 {
         bmplog("Pxxx wrong args");
         return None;
     }
     let reg = ascii_string_to_u32(args[0]);
     let value = ascii_string_to_u32(args[1]);
-    return Some( (reg,value));
+    return Some((reg, value));
 }
 // Write reg
-pub fn _P(command : &str, _args : &Vec<&str>) -> bool
-{
-    let reg : u32;
-    let val : u32;
-    match pp_prefix(&command[1..])
-    {
-        None =>  { encoder::simple_send("E01");return true },
-        Some( (x,y) )=>  {reg=x;val=y;},
-    }   
-    encoder::reply_bool(   bmp::bmp_write_register(reg,val) );
+pub fn _P(command: &str, _args: &Vec<&str>) -> bool {
+    let reg: u32;
+    let val: u32;
+    match pp_prefix(&command[1..]) {
+        None => {
+            encoder::simple_send("E01");
+            return true;
+        }
+        Some((x, y)) => {
+            reg = x;
+            val = y;
+        }
+    }
+    encoder::reply_bool(bmp::bmp_write_register(reg, val));
     return true;
 }
+// read 1 register
+pub fn _p(command: &str, _args: &Vec<&str>) -> bool {
+    let reg: u32 = crate::parsing_util::ascii_string_to_u32(&command[1..]);
+    match bmp::bmp_read_register(reg) {
+        Some(x) => encoder::simple_send_u32_le(x),
+        _ => encoder::reply_e01(),
+    }
+    true
+}
 
+// Read registers
+pub fn _g(_command: &str, _args: &Vec<&str>) -> bool {
+    let regs = crate::bmp::bmp_read_registers();
+    let mut e = encoder::new();
+
+    let mut buffer: [u8; 8] = [0; 8];
+    let n: usize = regs.len();
+    if n == 0 {
+        encoder::simple_send("0000");
+        true;
+    }
+    e.begin();
+    for i in 0..n {
+        e.add_u32_le(regs[i]);
+    }
+    // now read CSRs
+    e.end();
+    true
+}
--- rs/lnbmp/src/encoder.rs
+++ rs/lnbmp/src/encoder.rs
@@ -4,7 +4,7 @@
 //  or as hex encoded string
 //  aaa=>404040
 //
-// The code is designed to avoid allocating memory 
+// The code is designed to avoid allocating memory
 // AND avoid memcpying that 's why it may look a bit
 // complicated
 //
@@ -14,158 +14,171 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
-use crate::parsing_util::{u8_to_ascii,u8_to_ascii_to_buffer};
-use crate::{rngdb_send_data,rngdb_send_data_u8,rngdb_output_flush};
 use crate::packet_symbols;
-
+use crate::parsing_util::{u8_to_ascii, u8_to_ascii_to_buffer};
+use crate::{rngdb_output_flush, rngdb_send_data, rngdb_send_data_u8};
 
 // DF -- not thread safe, not re-entrant,ugly but simple
-const TEMP_BUFFER_SIZE : usize = 64;
-static mut  temp_buffer : [u8;TEMP_BUFFER_SIZE] = [0;TEMP_BUFFER_SIZE];
+const TEMP_BUFFER_SIZE: usize = 64;
+static mut temp_buffer: [u8; TEMP_BUFFER_SIZE] = [0; TEMP_BUFFER_SIZE];
 
 crate::setup_log!(false);
 
-pub struct encoder
-{
-    checksum : usize,
-    escaped  : bool,
+pub struct encoder {
+    checksum: usize,
+    escaped: bool,
 }
 
 // this buffer is NOT thread safe and should be used
 // only in a single shot
 
-fn get_temp_buffer() -> &'static mut [u8]
-{
+fn get_temp_buffer() -> &'static mut [u8] {
     unsafe {
-    return &mut temp_buffer;
+        return &mut temp_buffer;
     }
 }
 //
 //
-impl encoder
-{
+impl encoder {
     //
-    pub fn new() -> Self
-    {
-        encoder
-        {
-            checksum : 0,
-            escaped  : false,
+    pub fn new() -> Self {
+        encoder {
+            checksum: 0,
+            escaped: false,
         }
     }
-    pub fn reply_bool (status : bool)
-    {
-        if status
-        {
+    pub fn reply_bool(status: bool) {
+        if status {
             Self::reply_ok();
-        }
-        else
-        {
+        } else {
             Self::reply_e01();
         }
     }
-    pub fn reply_ok()
-    {
-        Self::raw_send_u8(b"$OK#9A"); Self::flush();
+    pub fn reply_ok() {
+        Self::raw_send_u8(b"$OK#9A");
+        Self::flush();
     }
-    pub fn reply_e01()
-    {
-        Self::raw_send_u8(b"$E01#A6"); Self::flush();
+    pub fn reply_e01() {
+        Self::raw_send_u8(b"$E01#A6");
+        Self::flush();
     }
     //
-    pub fn begin(&mut self)
-    {
+    pub fn add_u32(&mut self, val: u32) {
+        let mut tmp: [u8; 8] = [0; 8];
+
+        let mut shift = 24;
+        let mut offset = 0;
+        for i in 0..4 {
+            let digit: u8 = ((val >> shift) & 0xff) as u8;
+            crate::parsing_util::u8_to_ascii_to_buffer(digit, &mut tmp[offset..(offset + 2)]);
+            shift -= 8;
+            offset += 2;
+        }
+
+        self.add_u8(&tmp);
+    }
+    pub fn add_u32_le(&mut self, val: u32) {
+        let mut tmp: [u8; 8] = [0; 8];
+
+        let mut shift = 0;
+        let mut offset = 0;
+        for i in 0..4 {
+            let digit: u8 = ((val >> shift) & 0xff) as u8;
+            crate::parsing_util::u8_to_ascii_to_buffer(digit, &mut tmp[offset..(offset + 2)]);
+            shift += 8;
+            offset += 2;
+        }
+
+        self.add_u8(&tmp);
+    }
+    pub fn simple_send_u32_le(val: u32) {
+        let mut e = Self::new();
+
+        e.begin();
+        e.add_u32_le(val);
+        e.end();
+    }
+    //
+    pub fn begin(&mut self) {
         self.checksum = 0;
         self.escaped = false;
         Self::raw_send_u8(&[packet_symbols::CHAR_START]);
     }
     //
-    pub fn hexify_and_raw_send(str : &[u8])
-    {                
+    pub fn hexify_and_raw_send(str: &[u8]) {
         let buffer = get_temp_buffer();
         let mut byt = str;
-        while byt.len()>0
-        {
-            let n=core::cmp::min(byt.len(),TEMP_BUFFER_SIZE/2);
-            for i in 0..n
-            {
-                u8_to_ascii_to_buffer(byt[i],&mut buffer[2*i..]);
+        while byt.len() > 0 {
+            let n = core::cmp::min(byt.len(), TEMP_BUFFER_SIZE / 2);
+            for i in 0..n {
+                u8_to_ascii_to_buffer(byt[i], &mut buffer[2 * i..]);
             }
-            Self::raw_send_u8(&buffer[..2*n]);
-            byt=&byt[n..];
+            Self::raw_send_u8(&buffer[..2 * n]);
+            byt = &byt[n..];
         }
     }
     //
-    pub fn hex_and_add(&mut self, data :  &str)
-    {
+    pub fn hex_and_add(&mut self, data: &str) {
         let mut byt = data.as_bytes();
         let buffer = get_temp_buffer();
-        while byt.len()>0
-        {
-            let n=core::cmp::min(byt.len(),TEMP_BUFFER_SIZE/2);
-            for i in 0..n
-            {
-                u8_to_ascii_to_buffer(byt[i],&mut buffer[2*i..]);
+        while byt.len() > 0 {
+            let n = core::cmp::min(byt.len(), TEMP_BUFFER_SIZE / 2);
+            for i in 0..n {
+                u8_to_ascii_to_buffer(byt[i], &mut buffer[2 * i..]);
             }
-            self.add_u8(&buffer[..2*n]);
-            byt=&byt[n..];
+            self.add_u8(&buffer[..2 * n]);
+            byt = &byt[n..];
         }
     }
     //
-    pub fn add_u8(&mut self, byt :  &[u8])
-    {        
-        let l=byt.len();        
-        let mut n=0;
+    pub fn add_u8(&mut self, byt: &[u8]) {
+        let l = byt.len();
+        let mut n = 0;
         let buffer = get_temp_buffer();
 
-        for i in 0..l        
-        {
+        for i in 0..l {
             let c = byt[i];
-            match c
-            {
-                packet_symbols::CHAR_ESCAPE2 | packet_symbols::CHAR_ESCAPE | packet_symbols::CHAR_START | packet_symbols::CHAR_END =>
-                {
-                    buffer[n]=packet_symbols::CHAR_ESCAPE;
-                    buffer[n+1]=c^0x20;
-                    n+=2;
-                    self.checksum += (c^0x20) as usize;
+            match c {
+                packet_symbols::CHAR_ESCAPE2
+                | packet_symbols::CHAR_ESCAPE
+                | packet_symbols::CHAR_START
+                | packet_symbols::CHAR_END => {
+                    buffer[n] = packet_symbols::CHAR_ESCAPE;
+                    buffer[n + 1] = c ^ 0x20;
+                    n += 2;
+                    self.checksum += (c ^ 0x20) as usize;
                     self.checksum += packet_symbols::CHAR_ESCAPE as usize;
-                },
-             _ => {
-                    buffer[n]=c;                   
-                    n+=1;
+                }
+                _ => {
+                    buffer[n] = c;
+                    n += 1;
                     self.checksum += c as usize;
-             },
+                }
             };
-            if n> TEMP_BUFFER_SIZE-2
-            {
+            if n > TEMP_BUFFER_SIZE - 2 {
                 Self::raw_send_u8(&buffer[0..n]);
-                n=0;
+                n = 0;
             }
         }
-        if n>0
-        {
+        if n > 0 {
             Self::raw_send_u8(&buffer[0..n]);
         }
     }
     //
-    pub fn add(&mut self, data :  &str)
-    {
+    pub fn add(&mut self, data: &str) {
         self.add_u8(data.as_bytes());
     }
     //
-    pub fn end(&mut self)
-    {
+    pub fn end(&mut self) {
         // send end
         Self::raw_send_u8(&[packet_symbols::CHAR_END]);
         // Send checksum
         Self::raw_send_u8(&u8_to_ascii((self.checksum & 0xff) as u8));
         Self::flush();
     }
-    pub fn error(code : u8)
-    {
+    pub fn error(code: u8) {
         let mut e = Self::new();
-        let  zero: char  = (b'0'  + code) as char;
+        let zero: char = (b'0' + code) as char;
         let mut tmp = [0u8; 4];
         e.begin();
         e.add("E");
@@ -173,26 +186,22 @@ impl encoder
         e.end();
     }
     //
-    pub fn simple_send(data : &str)
-    {
+    pub fn simple_send(data: &str) {
         let mut e = Self::new();
         e.begin();
         e.add(data);
         e.end();
     }
     //
-    pub fn raw_send(data : &str)
-    {
+    pub fn raw_send(data: &str) {
         rngdb_send_data_u8(data.as_bytes());
     }
     //
-    pub fn raw_send_u8(data : &[u8])
-    {
+    pub fn raw_send_u8(data: &[u8]) {
         rngdb_send_data_u8(data);
-    }   
-    pub fn flush()
-    {
+    }
+    pub fn flush() {
         rngdb_output_flush();
     }
-    
 }
+
