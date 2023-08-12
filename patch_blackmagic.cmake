340a36e9b498ff7538de77f76f4e5c1ab268d532
mean <fixounet@free.fr>
2023-08-12 08:54:01 +0200

    riscv: add patch to enable ch32v3xx

--- patch_blackmagic.cmake
+++ patch_blackmagic.cmake
@@ -8,7 +8,9 @@ APPLY_PATCH_IF_NEEDED(patched9 ${BMP_PREFIX2}blackmagic_no_libopencm3.patch     
 APPLY_PATCH_IF_NEEDED(patched11 ${BMP_PREFIX2}blackmagic_use_embedded_printf.patch        ${BMP_PREFIX}blackmagic      "use embedded printf instead of bmp newlib ")
 #APPLY_PATCH_IF_NEEDED(patched12 ${BMP_PREFIX2}blackmagic_make_riscv32_register_public_add_single_write.patch        ${BMP_PREFIX}blackmagic      "expose riscv32 reg acces, add single write ")
 APPLY_PATCH_IF_NEEDED(patched13 ${BMP_PREFIX2}blackmagic_add_rvswd.patch                  ${BMP_PREFIX}blackmagic      "include rvswd.h")
-# ** APPLY_PATCH_IF_NEEDED(patched14 ${BMP_PREFIX2}blackmagic_use_embedded_printf2.patch       ${BMP_PREFIX}blackmagic      "use embedded printf (2) ")
+APPLY_PATCH_IF_NEEDED(patched14 ${BMP_PREFIX2}blackmagic_use_embedded_printf2.patch       ${BMP_PREFIX}blackmagic      "use embedded printf (2) ")
+APPLY_PATCH_IF_NEEDED(patched15 ${BMP_PREFIX2}blackmagic_enable_ch32v3xx.patch           ${BMP_PREFIX}blackmagic       "enable the ch32v3xx mcu  ")
+
 # ** APPLY_PATCH_IF_NEEDED(patched14 ${BMP_PREFIX2}blackmagic_undefined_debug_bmp.patch        ${BMP_PREFIX}blackmagic      "dont use bmp_debug.h")
 
 #APPLY_PATCH_IF_NEEDED(patched9 ${BMP_PREFIX2}blackmagic_intercept_commands.patch        ${BMP_PREFIX}blackmagic      "intercept gdb commands ")
