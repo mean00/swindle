2e33e22ce9c208fa1057150a63aeec201db5eccd
mean <fixounet@free.fr>
2023-09-23 10:06:24 +0200

    build: add pico

--- mcuSelect.cmake
+++ mcuSelect.cmake
@@ -24,10 +24,22 @@ IF(NOT DEFINED LN_ARCH)
         SET(LN_MCU_STATIC_RAM  7     CACHE INTERNAL "")
         #SET(LN_MCU_SPEED 72000000    CACHE INTERNAL "") #=> ok
         SET(LN_MCU_SPEED 96000000    CACHE INTERNAL "") #=> ok
+        
         #      SET(LN_MCU_SPEED  72000000    CACHE INTERNAL "") #=> ok
         SET(LN_SPEC         "picolibc"   CACHE INTERNAL "") # if not set we use nano
         SET(LN_ENABLE_USBD True CACHE INTERNAL "")
         SET(LN_BOOTLOADER_SIZE 8     CACHE INTERNAL "")
+    ELSEIF(USE_RP2040)
+        SET(LN_ARCH            "ARM" CACHE INTERNAL "")
+        SET(LN_MCU             "RP2040"  CACHE INTERNAL "")
+        SET(LN_MCU_RAM_SIZE    255    CACHE INTERNAL "")
+        SET(LN_MCU_SPEED       72000000       CACHE INTERNAL "")
+        SET(LN_MCU_STATIC_RAM  10     CACHE INTERNAL "")
+        SET(LN_MCU_FLASH_SIZE  2048   CACHE INTERNAL "")
+        SET(LN_MCU_EEPROM_SIZE 2     CACHE INTERNAL "")
+        SET(LN_BOOTLOADER_SIZE 8     CACHE INTERNAL "")        
+        SET(LN_SPEC            "picolibc"   CACHE INTERNAL "") # if not set we use nano
+        
     ELSE()
         SET(LN_ARCH            "ARM" CACHE INTERNAL "")
         SET(LN_MCU             "M3"  CACHE INTERNAL "")
