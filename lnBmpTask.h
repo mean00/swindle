2e38e27bb8e4ef3eda59440e72bb45f5ff7d2b02
mean <fixounet@free.fr>
2023-10-28 14:27:40 +0200

    bmp : shrink down stack allocated to try to fit in a regulat bluepill

--- lnBmpTask.h
+++ lnBmpTask.h
@@ -1,7 +1,13 @@
 #pragma ONCE
 
 #define TASK_BMP_SERIAL_PRIORITY 4
-#define TASK_BMP_SERIAL_STACK_SIZE (5000) // in bytes
+
 #define TASK_BMP_GDB_PRIORITY 5
-// #define TASK_BMP_GDB_STACK_SIZE     ((1680)/4) // in int32
-#define TASK_BMP_GDB_STACK_SIZE (8072) // in bytes
+
+#if 1 // small footprint, min heap = 21560
+    #define TASK_BMP_SERIAL_STACK_SIZE (1024) // in bytes
+    #define TASK_BMP_GDB_STACK_SIZE (3*1024) // in bytes
+#else // large fooprint min heap = 12584
+    #define TASK_BMP_SERIAL_STACK_SIZE (5000) // in bytes
+    #define TASK_BMP_GDB_STACK_SIZE (8072) // in bytes
+#endif
