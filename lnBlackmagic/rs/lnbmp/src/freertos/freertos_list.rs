8fae66d9215873a2139262d19adab45fd9465e3b
mean <fixounet@free.fr>
2023-11-22 07:11:16 +0100

    rs: FOS, change the way the lists are crawled

--- lnBlackmagic/rs/lnbmp/src/freertos/freertos_list.rs
+++ lnBlackmagic/rs/lnbmp/src/freertos/freertos_list.rs
@@ -1,3 +1,7 @@
+/*
+    https://aosabook.org/en/v2/freertos.html
+*/
+
 use alloc::vec::Vec;
 
 use crate::bmp::{bmp_read_mem,bmp_read_mem32};
@@ -6,20 +10,29 @@ crate::gdb_print_init!();
 use crate::{bmplog, bmpwarning};
 /**
  * \fn freertos_crawl_list
+ * \brief returns a list of items, in out case TCB *
  */
 pub fn freertos_crawl_list(address : u32) -> Vec<u32>
 {
     let mut v: Vec<u32>=Vec::new();    
-    let mut list_header : [u32;3]=[0,0,0];
+    let mut list_header : [u32;5]=[0,0,0,0,0];
 
     // Read the list head
+    //  0 #of items 
+    //  1 *pIndex = last item
+    //listEnd
+    //  2 item value (ffff)
+    //  3 *pxNext = start of list <= beignning of the list
+    //  4 *previous
+    //  5 *owner
+    //  6 *container
     if !bmp_read_mem32(address, &mut list_header) 
     {
         bmplog!("Fail to parse list at address 0x{:x}\n",address);
         return v;
     }
     let count = list_header[0];
-    let mut next = list_header[1];
+    let mut next = list_header[3];
 
     let mut items : [u32;5]=[0,0,0,0,0];
 
@@ -27,30 +40,18 @@ pub fn freertos_crawl_list(address : u32) -> Vec<u32>
     {
         // Read item
         // 0 number
-        // 1 next
-        // 2 previous
-        // 3 owner <- tcv
-        // 4 container
-        if !bmp_read_mem32(address, &mut items) 
+        // 1 *next
+        // 2 *previous
+        // 3 *owner <- tcb
+        // 4 *container
+        if !bmp_read_mem32(next, &mut items) 
         {
             bmplog!("Fail to parse list at address 0x{:x}\n",next);
             return v;
         }       
         next=items[1];
-
-        let state_list_item = items[3];
-        // owner is a StateListeItem
-        // 0 Item
-        // 1 next
-        // 2 Prev
-        // 3 owner <= the TCB is the actual owner
-        if !bmp_read_mem32(state_list_item, &mut items) 
-        {
-            bmplog!("Fail to parse list at address 0x{:x}\n",next);
-            return v;
-        }        
-        let top_of_stack = items[3];
-        v.push( top_of_stack);  
+        let tcb = items[3];        
+        v.push( tcb);  
     }
     v
 }
