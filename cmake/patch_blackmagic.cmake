1bb2eb3352f0379557bd0698eebcee5b3c99de4a
mean <fixounet@free.fr>
2024-01-09 07:18:58 +0100

    rv32 : add patch for riscv / ch32vxx

--- cmake/patch_blackmagic.cmake
+++ cmake/patch_blackmagic.cmake
@@ -7,6 +7,5 @@ APPLY_PATCH_IF_NEEDED2(patched9 ${BMP_PATCH_FOLDER}/blackmagic_no_libopencm3.pat
 APPLY_PATCH_IF_NEEDED2(patched10 ${BMP_PATCH_FOLDER}/blackmagic_make_swd_scan_public.patch       ${BMP_PREFIX}blackmagic   "make swdp_scan public ")
 APPLY_PATCH_IF_NEEDED2(patched11 ${BMP_PATCH_FOLDER}/blackmagic_use_embedded_printf.patch        ${BMP_PREFIX}blackmagic   "use embedded printf ")
 APPLY_PATCH_IF_NEEDED2(patched12 ${BMP_PATCH_FOLDER}/blackmagic_disable_cortexr.patch            ${BMP_PREFIX}blackmagic   "disable cortexr ")
-#APPLY_PATCH_IF_NEEDED2(patched13 ${BMP_PATCH_FOLDER}/blackmagic_rswd_support.patch               ${BMP_PREFIX}blackmagic   "add rvswd support (perigoso)")
-#APPLY_PATCH_IF_NEEDED2(patched14 ${BMP_PATCH_FOLDER}/blackmagic_rswd_support2.patch               ${BMP_PREFIX}blackmagic   "add rvswd support  command(perigoso)")
+APPLY_PATCH_IF_NEEDED2(patched13 ${BMP_PATCH_FOLDER}/blackmagic_rswd_support.patch               ${BMP_PREFIX}blackmagic   "add rvswd support (perigoso)")
 
