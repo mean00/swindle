3e1950f0ee6a3e632db2f62b9530fbe0ad10b60e
mean <fixounet@free.fr>
2024-01-28 11:07:01 +0100

    remote : skeleton host side

--- blackmagic_addon/hosted/remote_rv_protocol.c
+++ blackmagic_addon/hosted/remote_rv_protocol.c
@@ -14,6 +14,32 @@
 #include "target_internal.h"
 #include "riscv_debug.h"
 
+#include "gdb_packet.h"
+#include "bmp_remote.h"
+#include "remote/protocol_v2_defs.h"
+
+#define RPC_RV_PACKET       'B'
+
+#define RPC_RV_SCAN         's'
+#define RPC_RV_DM_READ      'r'
+#define RPC_RV_DM_WRITE     'w'
+
+extern void do_assert(const char *s);
+#define xAssert(x) if(!(x)) { do_assert(#x);}
+
+uint8_t reply[REMOTE_MAX_MSG_SIZE];
+
+
+static uint32_t from_ptr(const uint8_t *p)
+{
+     return ((uint32_t)(p[1])<<0) + 
+            ((uint32_t)(p[2])<<8) + 
+            ((uint32_t)(p[3])<<16) + 
+            ((uint32_t)(p[4])<<24) ;
+}
+
+#define SERIALIZE(X)  X&0xff, X>>8, X>>16, X>>24
+
 /**
  * @brief 
  * 
@@ -25,7 +51,22 @@
  */
  bool remote_ch32_riscv_dmi_read(riscv_dmi_s *dmi, uint32_t address, uint32_t *value)
  {
-    return false;
+    uint8_t buffer[] = {REMOTE_SOM, RPC_RV_PACKET, RPC_RV_DM_READ,  address};
+    platform_buffer_write(buffer, sizeof(buffer));
+    int length = platform_buffer_read(reply, REMOTE_MAX_MSG_SIZE);
+    if (length < 1)
+    {
+        DEBUG_ERROR("Invalid pin set reply\n");
+        return false;
+    }
+    bool r = reply[0] == REMOTE_RESP_OK;
+    if (!r)
+    {
+        DEBUG_ERROR(" dm_probe write error\n");
+        xAssert(0);
+    }    
+    *value  = from_ptr(reply+1);
+    return true;
  }
 /**
  * @brief 
@@ -38,7 +79,22 @@
  */
 bool remote_ch32_riscv_dmi_write(riscv_dmi_s *dmi, uint32_t address, uint32_t value)
 {
-return false;
+    uint8_t buffer[] = {REMOTE_SOM, RPC_RV_PACKET, RPC_RV_DM_WRITE,  address, SERIALIZE(value)};
+    platform_buffer_write(buffer, sizeof(buffer));
+    int length = platform_buffer_read(reply, REMOTE_MAX_MSG_SIZE);
+    if (length < 1)
+    {
+        DEBUG_ERROR("Invalid pin set reply\n");
+        return false;
+    }
+    bool r = reply[0] == REMOTE_RESP_OK;
+    if (!r)
+    {
+        DEBUG_ERROR(" dm_probe write error\n");
+        xAssert(0);
+    }
+
+    return r;
 }
 /**
  * @brief 
@@ -49,6 +105,21 @@ return false;
  */
 bool remote_rv_dm_probe(uint32_t *id)
 {
-    return false;
+    uint8_t buffer[] = {REMOTE_SOM, RPC_RV_PACKET, RPC_RV_SCAN,  REMOTE_EOM};
+    platform_buffer_write(buffer, sizeof(buffer));
+    int length = platform_buffer_read(reply, REMOTE_MAX_MSG_SIZE);
+    if (length < 1)
+    {
+        DEBUG_ERROR("Invalid pin set reply\n");
+        return false;
+    }
+    bool r = reply[0] == REMOTE_RESP_OK;
+    if (!r)
+    {
+        DEBUG_ERROR(" dm_probe set error\n");
+        xAssert(0);
+    }
+    *id  = from_ptr(reply+1);
+    return true;
 }
  // EOF
