625f7466fb28517578eb7fa3a70a731fbf8247ff
mean <fixounet@free.fr>
2024-02-01 07:48:44 +0100

    build: g32, increase resever ram due to larger usb buffers

--- mcuSelect.cmake
+++ mcuSelect.cmake
@@ -23,7 +23,7 @@ IF(NOT DEFINED LN_ARCH)
         SET(LN_MCU             "M4"  CACHE INTERNAL "")
         SET(LN_MCU_FLASH_SIZE  256   CACHE INTERNAL "")
         SET(LN_MCU_RAM_SIZE    46    CACHE INTERNAL "")
-        SET(LN_MCU_STATIC_RAM  7     CACHE INTERNAL "")
+        SET(LN_MCU_STATIC_RAM  8     CACHE INTERNAL "")
         #SET(LN_MCU_SPEED 72000000    CACHE INTERNAL "") #=> ok
         SET(LN_MCU_SPEED 96000000    CACHE INTERNAL "") #=> ok
         
@@ -43,7 +43,7 @@ IF(NOT DEFINED LN_ARCH)
         SET(LN_BOOTLOADER_SIZE 8     CACHE INTERNAL "")        
         SET(LN_SPEC            "picolibc"   CACHE INTERNAL "") # if not set we use nano
         
-    ELSE()
+ELSE() # Small bluepill style
         SET(LN_ARCH            "ARM" CACHE INTERNAL "")
         SET(LN_MCU             "M3"  CACHE INTERNAL "")
         SET(LN_MCU_RAM_SIZE    20    CACHE INTERNAL "")
@@ -54,8 +54,8 @@ IF(NOT DEFINED LN_ARCH)
           SET(LN_MCU_FLASH_SIZE  64   CACHE INTERNAL "")
           SET(LN_MCU_EEPROM_SIZE 2    CACHE INTERNAL "")
         ELSE() # "Big" flash
-          SET(LN_MCU_STATIC_RAM  6     CACHE INTERNAL "")
-          SET(LN_MCU_FLASH_SIZE  128   CACHE INTERNAL "")
+          SET(LN_MCU_STATIC_RAM  7     CACHE INTERNAL "")
+          SET(LN_MCU_FLASH_SIZE  188   CACHE INTERNAL "")
           SET(LN_MCU_EEPROM_SIZE 2     CACHE INTERNAL "")
           SET(LN_BOOTLOADER_SIZE 8     CACHE INTERNAL "")
           SET(LN_ENABLE_USBD True CACHE INTERNAL "")
