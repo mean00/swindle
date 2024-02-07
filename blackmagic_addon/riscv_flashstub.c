55ce61f0292da01295c7019c83f9bd8fcc9a57c1
mean <fixounet@free.fr>
2024-02-07 20:11:28 +0100

    flashstub : de doxygenify

--- blackmagic_addon/riscv_flashstub.c
+++ blackmagic_addon/riscv_flashstub.c
@@ -1,4 +1,23 @@
-
+/*
+ * This file is part of the Black Magic Debug project.
+ *
+ * Copyright (C) 2011  Black Sphere Technologies Ltd.
+ * Written by Gareth McMullin <gareth@blacksphere.co.nz>
+ * Copyright (C) 2022-2023 1BitSquared <info@1bitsquared.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "general.h"
 #include "riscv_debug.h"
 #include "target.h"
@@ -11,8 +30,8 @@
     {                                                                                                                  \
     }
 #endif
-/**
-    \brief Execute code on the target to speed up flash operation
+/*
+    Execute code on the target to speed up flash operation
     at the end of the execution, the code jumps back to the beginning of the RAM where
     we put a breakpoint in _prepare.
 */
