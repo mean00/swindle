ac3934e97aa61f794ed70f44ca699d85d9c6b014
mean <fixounet@free.fr>
2024-06-09 11:14:42 +0200

    update dfu update for ch32

--- dfu_ch32_real.sh
+++ dfu_ch32_real.sh
@@ -1,1 +1,1 @@
-sudo dfu-util -v -d 1d50:6030 -s 0x0004000:leave -D swindle_CH32V3x_144M_CLANG.elf.ck_bin
+sudo dfu-util -v -d 1d50:6030 -s 0x0004000:leave -D swindle_CH32V3x_144Mhz_CLANG.elf.ck_bin
