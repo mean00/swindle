9215c895e2e53c12bb78c0d3b526016124ccacdd
mean <fixounet@free.fr>
2024-08-06 07:38:56 +0200

    fix mon reset return

--- swindle/rs/rs_swindle/src/commands/mon.rs
+++ swindle/rs/rs_swindle/src/commands/mon.rs
@@ -148,7 +148,7 @@ pub fn _target_reset(_command: &str, _args: &[&str]) -> bool {
     #[cfg(not(feature = "hosted"))]
     rnarduino::rn_os_helper::delay_ms(20);
     bmp::bmp_platform_nrst_set_val(false);
-    encoder::reply_e01();
+    encoder::reply_ok();
     true
 }
 
