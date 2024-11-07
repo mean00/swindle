3e69ee2440679e9c15a7d290be1f3045ab2afcd9
mean <fixounet@free.fr>
2024-11-07 07:53:27 +0100

    update patches etc...

--- cmake/patch_blackmagic.cmake
+++ cmake/patch_blackmagic.cmake
@@ -1,12 +1,8 @@
 
 include(applyPatch)
-#APPLY_PATCH_IF_NEEDED3(patched6 ${BMP_PATCH_FOLDER}/blackmagic_small_exception.patch             ${LNBMP_TOP_FOLDER}   "use smaller setjmp/long jmp on smaller arm")
-#APPLY_PATCH_IF_NEEDED3(patched16 ${BMP_PATCH_FOLDER}/blackmagic_make_ebreak_work.patch           ${LNBMP_TOP_FOLDER}   "riscv make sw breakpoint work")
-#APPLY_PATCH_IF_NEEDED3(patched18 ${BMP_PATCH_FOLDER}/blackmagic_riscv_stub.patch                 ${LNBMP_TOP_FOLDER}   "declare flashstub function")
-
 APPLY_PATCH_IF_NEEDED3(patched8 ${BMP_PATCH_FOLDER}/blackmagic_no_debug_bmp.patch                ${LNBMP_TOP_FOLDER}   "undefined bmp_debug removal")
 APPLY_PATCH_IF_NEEDED3(patched7 ${BMP_PATCH_FOLDER}/blackmagic_use_custom_target_mem_map.patch   ${LNBMP_TOP_FOLDER}   "use heap based mem map")
-APPLY_PATCH_IF_NEEDED3(patched9 ${BMP_PATCH_FOLDER}/blackmagic_no_libopencm3.patch               ${LNBMP_TOP_FOLDER}   "dont use opencm3 ")
+# obsolete  APPLY_PATCH_IF_NEEDED3(patched9 ${BMP_PATCH_FOLDER}/blackmagic_no_libopencm3.patch               ${LNBMP_TOP_FOLDER}   "dont use opencm3 ")
 APPLY_PATCH_IF_NEEDED3(patched10 ${BMP_PATCH_FOLDER}/blackmagic_make_swd_scan_public.patch       ${LNBMP_TOP_FOLDER}   "make swdp_scan public ")
 APPLY_PATCH_IF_NEEDED3(patched11 ${BMP_PATCH_FOLDER}/blackmagic_use_embedded_printf.patch        ${LNBMP_TOP_FOLDER}   "use embedded printf ")
 APPLY_PATCH_IF_NEEDED3(patched12 ${BMP_PATCH_FOLDER}/blackmagic_disable_cortexr.patch            ${LNBMP_TOP_FOLDER}   "disable cortexr ")
