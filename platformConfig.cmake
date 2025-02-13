93e93cd9277e17abcfbfff049814bf6ed78da507
mean <fixounet@free.fr>
2025-02-13 08:33:12 +0100

    cmake cosmetic

--- platformConfig.cmake
+++ platformConfig.cmake
@@ -1,13 +1,13 @@
 IF(WIN32)
   SET(PLATFORM_TOOLCHAIN_SUFFIX ".exe")
-ENDIF(WIN32)
+ENDIF()
 
 IF("${LN_ARCH}" STREQUAL "RISCV") # RISCV
   IF( ${LN_MCU} STREQUAL "CH32V3x")    #----------CH32V307
     #SET(PLATFORM_PREFIX riscv-none-embed-) # MRS toolchain
     IF(WIN32)
       SET(PLATFORM_TOOLCHAIN_PATH todo_todo) # Use /c/foo or c:\foo depending if you use mingw cmake or win32 cmake
-    ELSE(WIN32)
+    ELSE()
       #--- GCC ---------
       SET(PLATFORM_TOOLCHAIN_PATH "/riscv/tools5/bin" CACHE INTERNAL "")
       IF(USE_HW_FPU)
@@ -33,15 +33,15 @@ IF("${LN_ARCH}" STREQUAL "RISCV") # RISCV
         #SET(PLATFORM_CLANG_SYSROOT "${PLATFORM_CLANG_PATH}/../lib/clang-runtimes/riscv32-none-eabi/riscv32_soft_nofp/" CACHE INTERNAL "")
         SET(PLATFORM_CLANG_C_FLAGS "--target=riscv32 -march=rv32imac -mabi=ilp32  " CACHE INTERNAL "")
       ENDIF()
-    ENDIF(WIN32)
-  ELSE( ${LN_MCU} STREQUAL "CH32V3x") #----------- GD32VF103
+    ENDIF()
+  ELSE() #----------- GD32VF103
     SET(PLATFORM_PREFIX riscv32-unknown-elf-)
     SET(PLATFORM_C_FLAGS "-march=rv32imac -mabi=ilp32 ")
     IF(WIN32)
       SET(PLATFORM_TOOLCHAIN_PATH /c/gd32/toolchain/bin/) # Use /c/foo or c:\foo depending if you use mingw cmake or win32 cmake
-    ELSE(WIN32)
+    ELSE()
       SET(PLATFORM_TOOLCHAIN_PATH /opt/gd32/toolchain/bin/)
-    ENDIF(WIN32)
+    ENDIF()
   ENDIF()
 ELSE()
   SET(PLATFORM_PREFIX arm-none-eabi-)
