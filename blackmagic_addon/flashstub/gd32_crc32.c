18aadcbb7bbcdb192c274aff215c4e72d019538f
mean <fixounet@free.fr>
2025-04-27 16:17:34 +0200

    gd32 : optimize CRC32

--- blackmagic_addon/flashstub/gd32_crc32.c
+++ blackmagic_addon/flashstub/gd32_crc32.c
@@ -1,7 +1,15 @@
+/*
+ *    This is a hw assisted crc32 as use by gdb through compare-sections
+ *    The byte order is reversed compared to the native way
+ *    ( the flashstub can only process 32 bits aligned data , which is fairly normal)
+ *
+ *
+ */
+
 #include "stdint.h"
 #include "stub.h"
-#define CH32_CRC32_ADDR 0x40023000
-#define CH32_CRC32_CONTROL_RESET 1
+#define GD32_CRC32_ADDR 0x40023000
+#define GD32_CRC32_CONTROL_RESET 1
 #define AHBPCENR (*(uint32_t *)0x40021014UL)
 typedef struct
 {
@@ -12,46 +20,38 @@ typedef struct
 
 typedef volatile CRC_IPx CRC_IP;
 
-#if 0
-static uint32_t  complete_crc32(uint8_t *data, uint32_t len, uint32_t crc) 
-{
-   for(int i=0;i<len;i++)
-   {
-      uint32_t next = data[i];
-      crc = crc ^ next;
-      for (int j = 7; j >= 0; j--) {
-         uint32_t mask = -(crc & 1);
-         crc = (crc >> 1) ^ (0xEDB88320 & mask);
-      }      
-   }
-   return ~crc;
-}
-#endif
 /**
- *  crc is in A2
- *  __attribute__((naked))
+ *  crc output is in r1
  */
 void __attribute__((naked)) __attribute__((noreturn)) ch32_cr(uint32_t addr, uint32_t len_in_u32, uint32_t *output)
 {
     // Enable  CRC clock
     AHBPCENR |= 1 << 6;
     //
-    CRC_IP *crc = (CRC_IP *)CH32_CRC32_ADDR;
+    CRC_IP *crc = (CRC_IP *)GD32_CRC32_ADDR;
     // reset writes 0xFFFFFFF by itself
-    crc->control = CH32_CRC32_CONTROL_RESET;
+    crc->control = GD32_CRC32_CONTROL_RESET;
     // crc->data = init;
     uint32_t *mem = (uint32_t *)addr;
     uint32_t *lim = mem + len_in_u32;
+    while (mem + 32 < lim)
+    {
+        crc->data = __builtin_bswap32(mem[0]);
+        crc->data = __builtin_bswap32(mem[1]);
+        crc->data = __builtin_bswap32(mem[2]);
+        crc->data = __builtin_bswap32(mem[3]);
+        crc->data = __builtin_bswap32(mem[4]);
+        crc->data = __builtin_bswap32(mem[5]);
+        crc->data = __builtin_bswap32(mem[6]);
+        crc->data = __builtin_bswap32(mem[7]);
+        mem += 8;
+    }
     while (mem < lim)
     {
-        uint32_t in = *(mem++);
-        uint32_t out =
-            ((in >> 24) & 0xff) | (((in >> 16) & 0xff) << 8) | (((in >> 8) & 0xff) << 16) | ((in & 0xff) << 24);
-        // uint32_t out= __builtin_bswap32(in);
-        crc->data = out;
+        crc->data = __builtin_bswap32(*(mem++));
     }
-    __asm__("mov r1,%0" ::"r"(crc->data));
-    //*output = crc->data;
+    uint32_t out = crc->data;
+    __asm__("mov r1,%0" ::"r"(out));
     stub_exit(0);
     while (1)
     {
