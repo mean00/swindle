943b3141c7fc8a512ac17895f830aa3da2b6eddc
mean <fixounet@free.fr>
2025-06-09 09:41:29 +0200

    fixup hosted mode, update to new riscv compiler, update esprit

--- swindle/rs/rs_swindle/private_include/bindgen.sh
+++ swindle/rs/rs_swindle/private_include/bindgen.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 set -x
 #
-gen_cpp()
-{
-bash ../../../../lnArduino/cmake/rustgen.sh  ${1}   ${2} $PWD
+gen_cpp() {
+  bash ../../../../esprit/cmake/rustgen.sh ${1} ${2} $PWD
 }
 
-gen_c()
-{
-bash ../../../../lnArduino/cmake/rustgen_c.sh  ${1}   ${2} $PWD
+gen_c() {
+  bash ../../../../esprit/cmake/rustgen_c.sh ${1} ${2} $PWD
 }
-
 
 gen_c bmp_command.h ../src/rn_bmp_cmd_c.rs
-
-
