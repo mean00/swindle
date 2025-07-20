ebdd4ff999ec0b4cbdd205d2728429ad006d5745
mean <fixounet@free.fr>
2025-07-20 15:24:26 +0200

    build: update local configuration with pico sdk path

--- platformConfig.cmake
+++ platformConfig.cmake
@@ -60,7 +60,7 @@ ELSE()
     #SET(PLATFORM_CLANG_PATH  /opt/llvm_arm/14/bin)
     #SET(PLATFORM_CLANG_VERSION "")
     SET(PLATFORM_TOOLCHAIN_PATH "/arm/14.2/bin/")
-    SET(PICO_SDK_PATH /pico/pico-sdk2 CACHE INTERNAL "") # For PICO2040
+    SET(PICO_SDK_PATH /opt/pico/pico-sdk CACHE INTERNAL "") # For PICO2040
 
   ENDIF()
 ENDIF()
