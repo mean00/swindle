21469cdeda27b255e40b9c1a9b01f2bced6a8ecc
mean <fixounet@free.fr>
2025-08-11 08:30:16 +0200

    gpio: add the inverted reset case

--- swindle/private_include/lnBMP_swdio_common.h
+++ swindle/private_include/lnBMP_swdio_common.h
@@ -6,6 +6,48 @@
  * @brief
  *
  */
+// In case of inverted reset, writting 1 pulls reset to zero
+// It is a digital pin
+#ifdef SWINDLE_INVERTED_RESET
+class SwdReset
+{
+  public:
+    SwdReset(lnBMPPins no)
+    {
+        _me = _mapping[no];
+        _state = false;
+        // 1: Hi Z
+        // 0: GND
+        lnDigitalWrite(_me, 0);
+        lnPinMode(_me, lnOUTPUT);
+    }
+
+    void on()
+    {
+        _state = true;
+        lnDigitalWrite(_me, 1);
+    }
+    void hiZ()
+    {
+        lnDigitalWrite(_me, 0);
+    }
+    void off()
+    {
+        _state = false;
+        lnDigitalWrite(_me, 0);
+    }
+    bool state()
+    {
+        return _state;
+    }
+
+  protected:
+    lnPin _me;
+    bool _state;
+};
+#else
+// It is slightly different for non inverted reset
+// in that case the gpio is an open drain
 class SwdReset
 {
   public:
@@ -42,7 +84,7 @@ class SwdReset
     lnPin _me;
     bool _state;
 };
-
+#endif
 /**
  */
 class SwdWaitPin : public SwdPin
