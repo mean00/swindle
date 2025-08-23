9667f8050657fecae31a77e754e27f1e0717bcaf
mean <fixounet@free.fr>
2025-08-23 16:38:50 +0200

    ch32: reduce eeprom size to 1k, update dfu_ch32.sh

--- mcuSelect.cmake
+++ mcuSelect.cmake
@@ -6,15 +6,16 @@ IF(NOT DEFINED LN_ARCH)
     #SET(LN_USE_INTERNAL_CLOCK True        CACHE INTERNAL "")
     #SET(LN_MCU_SPEED          48000000    CACHE INTERNAL "") # 96 Mhz
     #SET(LN_MCU_SPEED          96000000    CACHE INTERNAL "") # 96 Mhz
-    SET(LN_MCU_SPEED          144000000    CACHE INTERNAL "") # 144 Mhz
+    SET(LN_MCU_SPEED          144000000   CACHE INTERNAL "") # 144 Mhz
     SET(LN_ARCH "RISCV"                   CACHE INTERNAL "")
     SET(LN_MCU  "CH32V3x"                 CACHE INTERNAL "")
     SET(LN_MCU_RAM_SIZE       64          CACHE INTERNAL "")
     SET(LN_MCU_FLASH_SIZE     256         CACHE INTERNAL "")
-    SET(LN_MCU_STATIC_RAM     13           CACHE INTERNAL "")
+    SET(LN_MCU_STATIC_RAM     13          CACHE INTERNAL "")
     SET(LN_SPEC               "picolibc"  CACHE INTERNAL "") # if not set we use nano
-    SET(LN_BOOTLOADER_SIZE     16          CACHE INTERNAL "")
-    SET(LN_USB_NB_CDC 3 CACHE INTERNAL "") # 2 CDC interfaces
+    SET(LN_BOOTLOADER_SIZE    16          CACHE INTERNAL "")
+    SET(LN_USB_NB_CDC         3           CACHE INTERNAL "") # 2 CDC interfaces
+    SET(LN_MCU_EEPROM_SIZE    1           CACHE INTERNAL "")
     #SET(LN_SPEC         "picolibc"   CACHE INTERNAL "") # if not set we use nano
   ELSE()
     IF(USE_GD32F3)
