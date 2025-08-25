8eb5cfd965f9c7c6edcfef29f7371c0286d0f27c
mean <fixounet@free.fr>
2025-08-25 08:14:32 +0200

    increase stack again

--- lnBmpTask.h
+++ lnBmpTask.h
@@ -5,8 +5,8 @@
 #define TASK_BMP_GDB_PRIORITY 5
 
 #if 1                                      // small footprint, min heap = 21560
-#define TASK_BMP_SERIAL_STACK_SIZE (3*1024)  // in bytes
-#define TASK_BMP_GDB_STACK_SIZE (3*1024) // in bytes
+#define TASK_BMP_SERIAL_STACK_SIZE (5*1024)  // in bytes
+#define TASK_BMP_GDB_STACK_SIZE (5*1024) // in bytes
 #else                                      // large fooprint min heap = 12584
 #define TASK_BMP_SERIAL_STACK_SIZE (5000)  // in bytes
 #define TASK_BMP_GDB_STACK_SIZE (8072)     // in bytes
