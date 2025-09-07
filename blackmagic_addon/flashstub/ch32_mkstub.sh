e0d27480ce31cafa63ee34df6d445cfe721860ed
mean <fixounet@free.fr>
2025-09-07 11:09:15 +0200

    ch32v3: make flashstub const

--- blackmagic_addon/flashstub/ch32_mkstub.sh
+++ blackmagic_addon/flashstub/ch32_mkstub.sh
@@ -3,14 +3,16 @@ export TOOLCHAIN=/riscv/llvm_19.1/bin/
 CC=${TOOLCHAIN}/clang-19
 OBJCOPY=${TOOLCHAIN}/llvm-objcopy
 OBJDUMP=${TOOLCHAIN}/llvm-objdump
-SYS=${TOOLCHAIN}/../lib/clang-runtimes/riscv32-unknown-elf/riscv32_hard_fp/include
+SYS=${TOOLCHAIN}/../lib/clang-runtimes/riscv32-unknown-elf/rv32imafc-zicsr-zifencei_hard_fp/include
 build_stub() {
   rm -f $1.o
   set -x
   $CC $1.c -g -O2 -o $1.o -nostdlib -I ../../blackmagic/src/target -I ../../blackmagic/src/include -I ${SYS}
   $OBJCOPY -Obinary $1.o $1.bin
   $OBJDUMP -S $1.o >$1.asm
-  xxd -i $1.bin >$1.stub
+  xxd -i $1.bin >$1.tmp
+  cat $1.tmp | sed 's/unsigned char/const unsigned char/g' | head -c -2 >$1.stub
+  echo ";" >>$1.stub
 }
 
 build_stub ch32v3x_erase
