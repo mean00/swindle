334f118a49f1c4f0ef6c0bfd44a947f5a66ba9d7
mean <fixounet@free.fr>
2025-10-20 08:10:08 +0200

    flashstub: for ch32vxx build with no hw fpu so it works with ch32v2xx also

--- blackmagic_addon/flashstub/ch32_mkstub.sh
+++ blackmagic_addon/flashstub/ch32_mkstub.sh
@@ -3,11 +3,14 @@ export TOOLCHAIN=/riscv/llvm_19.1/bin/
 CC=${TOOLCHAIN}/clang-19
 OBJCOPY=${TOOLCHAIN}/llvm-objcopy
 OBJDUMP=${TOOLCHAIN}/llvm-objdump
-SYS=${TOOLCHAIN}/../lib/clang-runtimes/riscv32-unknown-elf/rv32imafc-zicsr-zifencei_hard_fp/include
+GCC=/riscv/xpack-14.2.0-2/bin/riscv-none-elf-gcc
+LLD=${TOOLCHAIN}/ld.lld
+SYS=${TOOLCHAIN}/../lib/clang-runtimes/riscv32-unknown-elf/rv32imac-zicsr-zifencei_soft_nofp/include
 build_stub() {
   rm -f $1.o
   set -x
-  $CC $1.c -g -O2 -o $1.o -nostdlib -I ../../blackmagic/src/target -I ../../blackmagic/src/include -I ${SYS}
+  $CC $1.c -g -O2 -o $1.o -nostdlib -I ../../blackmagic/src/target -I ../../blackmagic/src/include -I ${SYS} -I$PWD
+  $GCC $1.c -o $1.elf -nostdlib -T ch32.ld -I ../../blackmagic/src/target -I ../../blackmagic/src/include -I ${SYS} -I$PWD
   $OBJCOPY -Obinary $1.o $1.bin
   $OBJDUMP -S $1.o >$1.asm
   xxd -i $1.bin >$1.tmp
