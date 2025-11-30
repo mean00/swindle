60714f5b7971ca8c5002c6496ee022a072d74c9a
mean <fixounet@free.fr>
2025-11-30 20:00:34 +0100

    ch32: more ethernet swindle

--- swindle/src/bmp_net.cpp
+++ swindle/src/bmp_net.cpp
@@ -13,6 +13,12 @@ extern "C"
 #include "version.h"
 }
 #include "lnLWIP.h"
+//
+#warning FIXME
+#define DHCP_LED PA15
+//
+#include "lnSocketRunner.h"
+//
 extern "C" void pins_init();
 extern void serialInit();
 extern void bmp_io_begin_session();
@@ -49,134 +55,57 @@ extern "C"
     void rngdbstub_run(uint32_t s, const uint8_t *d);
     void rngdbstub_poll();
 }
-
-#if 0
-#define GDB_BUFFER_SIZE 1024
-uint8_t gdb_buffer[GDB_BUFFER_SIZE];
-lnUsbCDC *uartCdc = NULL;
-
-class BufferGdb
-{
-  public:
-    BufferGdb(int instance)
-    {
-        _instance = instance;
-        _cdc = NULL;
-        _cdc = new lnUsbCDC(_instance);
-        _cdc->setEventHandler(gdbCdcEventHandler, this);
-        _eventGroup = new lnFastEventGroup();
-        _inSession = false;
-    }
-    bool inSession()
-    {
-        return _inSession;
-    }
-
-    lnUsbCDC *cdc()
-    {
-        xAssert(_cdc);
-        return _cdc;
-    }
-
-    bool takeOwnership()
-    {
-
-        _eventGroup->takeOwnership();
-        return true;
-    }
-    static void gdbCdcEventHandler(void *cookie, int interface, lnUsbCDC::lnUsbCDCEvents event, uint32_t payload)
-    {
-        BufferGdb *bg = (BufferGdb *)cookie;
-        xAssert(interface == bg->_instance);
-        bg->cdcEventHandler(event);
-    }
-    uint32_t waitEvents()
-    {
-        return _eventGroup->waitEvents(0xffff, GDB_MAX_POLLING_PERIOD);
-    }
-    void cdcEventHandler(lnUsbCDC::lnUsbCDCEvents event)
-    {
-        switch (event)
-        {
-        case lnUsbCDC::CDC_SET_SPEED:
-            break;
-        case lnUsbCDC::CDC_WRITE_AVAILABLE: // ignore
-            break;
-        case lnUsbCDC::CDC_DATA_AVAILABLE:
-            xAssert(_eventGroup);
-            _eventGroup->setEvents(GDB_CDC_DATA_AVAILABLE);
-            break;
-        case lnUsbCDC::CDC_SESSION_START:
-            bmp_io_begin_session();
-            _inSession = true;
-            xAssert(_eventGroup);
-            _eventGroup->setEvents(GDB_SESSION_START);
-            break;
-        case lnUsbCDC::CDC_SESSION_END:
-            bmp_io_end_session();
-            _inSession = false;
-            xAssert(_eventGroup);
-            _eventGroup->setEvents(GDB_SESSION_END);
-            break;
-        default:
-            xAssert(0);
-            break;
-        }
-    }
-
-  public:
-    int _instance;
-
-  protected:
-    bool _inSession;
-    lnUsbCDC *_cdc;
-    lnFastEventGroup *_eventGroup;
-};
-
-//
-BufferGdb *usbGdb = NULL;
-#endif
-lnSocket *current_connection = NULL;
-bool connected = false;
-
-void netCb(lnLwipEvent evt, void *arg)
-{
-    switch (evt)
-    {
-    case LwipReady:
-        Logger("DHCP up\n");
-        connected = true;
-        break;
-    default:
-        Logger(" ????? event \n");
-        break;
-    }
-}
+//--
 /**
  *
- * @param evt [TODO:parameter]
- * @param arg [TODO:parameter]
  */
-void sockCb(lnSocketEvent evt, void *arg)
+#define NET_GDB_BUFFER_SIZE 1500
+
+#if 0
+#define DEBUGME Logger
+#else
+#define DEBUGME(...)                                                                                                   \
+    {                                                                                                                  \
+    }
+#endif
+
+class socketRunnerGdb : public socketRunner
 {
-    printf(" Socket callback , event = 0x%x\n", evt);
-}
+  protected:
+    void process_incoming_data()
+    {
+        uint32_t lp = 0;
+        while (1)
+        {
+            uint32_t rd = 0;
+            DEBUGME("r?\n");
+            if (readData(NET_GDB_BUFFER_SIZE, _buffer, rd))
+            {
+                if (!rd) // no more data available
+                    return;
+                DEBUGME("r!%d\n", rd);
+                rngdbstub_run(rd, _buffer);
+                rngdbstub_poll(); // if we are un run mode, check if the target reached a breakpoint/watchpoint/...
+                DEBUGME("d%d\n", lp++);
+                // DEBUGME("Write done\n");
+            }
+        }
+    xit:
+        flushWrite();
+    }
+
+  protected:
+    uint8_t _buffer[NET_GDB_BUFFER_SIZE];
+};
+socketRunnerGdb *runnerGdb = NULL;
+
 /**
  * @brief [TODO:description]
  *
  * @return [TODO:return]
  */
-extern "C" int gdb_if_init(void)
+extern "C" int gdb_network_init(void)
 {
-    Logger("Starting Network\n");
-    lnLWIP::start(netCb, NULL);
-    while (!connected)
-    {
-        lnDelayMs(20);
-    }
-    current_connection = lnSocket::create(2000, sockCb, NULL);
-    // usbGdb = new BufferGdb(0);
-
     return 0;
 }
 /**
@@ -185,11 +114,15 @@ extern "C" int gdb_if_init(void)
 void initFreeRTOS()
 {
 }
+void gdb_if_init()
+{
+}
 /**
  * @brief [TODO:description]
  *
  * @param parameters [TODO:parameter]
  */
+
 void gdb_task(void *parameters)
 {
     (void)parameters;
@@ -201,33 +134,9 @@ void gdb_task(void *parameters)
     //
     swindle_init_rtt();
     rngdbstub_init();
-    while (!connected)
-    {
-        lnDelayMs(10);
-    }
-    lnDelayMs(10);
-    uint8_t tmp[256];
-    while (1)
-    {
-    again:
-        if (current_connection->accept() == lnSocket::Ok)
-        {
-            while (1)
-            {
-                uint32_t nb;
-                if (current_connection->read(256, tmp, nb) == lnSocket::Ok)
-                {
-                    rngdbstub_run(nb, tmp);
-                    rngdbstub_poll(); // if we are un run mode, check if the target reached a breakpoint/watchpoint/...
-                }
-                else
-                {
-                    current_connection->close();
-                    goto again;
-                }
-            }
-        }
-    }
+
+    runnerGdb = new socketRunnerGdb;
+    runnerGdb->run();
 }
 /**
  * @brief [TODO:description]
@@ -247,24 +156,13 @@ void debug_serial_send_stdout(const uint8_t *const data, const size_t len)
  */
 extern "C" void rngdb_send_data_c(uint32_t sz, const uint8_t *ptr)
 {
-    uint32_t o;
-    while (sz)
-    {
-        if (lnSocket::Ok != current_connection->write(sz, ptr, o))
-        {
-            // TODO disconenct
-            Logger("Write error\n");
-            return;
-        }
-        sz -= o;
-        ptr += o;
-    }
+    runnerGdb->writeData(sz, ptr);
 }
 /**
  * @brief [TODO:description]
  */
 extern "C" void rngdb_output_flush_c()
 {
-    current_connection->flush();
+    runnerGdb->flushWrite();
 }
 // EOF
