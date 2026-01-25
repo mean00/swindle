7e7a240a6f4561f87bd324e2a9ec898392fc7c83
mean <fixounet@free.fr>
2026-01-25 17:15:39 +0100

    rp2040: use swd template, adapt PIO accordingly

--- swindle/src/rp2040/bmp_swdTap_rp2040.cpp
+++ swindle/src/rp2040/bmp_swdTap_rp2040.cpp
@@ -90,142 +90,15 @@ static uint32_t zread(uint32_t size)
     return value >> (32 - size);
 }
 
-/**
-    \fn ln_adiv5_swd_write_no_check
- */
-extern "C" bool ln_adiv5_swd_write_no_check(const uint16_t addr, const uint32_t data)
-{
-    uint8_t request = make_packet_request(ADIV5_LOW_WRITE, addr);
-    xsm->waitTxEmpty();
-    zwrite(8, request);
-    uint32_t ack = zread(3);
-    bool parity = lnOddParity(data);
-    zwrite(2, 0x03);
-    zwrite(32, data);
-    xsm->waitTxEmpty();
-    zwrite(1, parity);
-    zwrite(8, 0);
-    return ack != SWD_ACK_OK;
-}
-
-/**
-        \fn ln_adiv5_swd_read_no_check
-
- */
-extern "C" uint32_t ln_adiv5_swd_read_no_check(const uint16_t addr)
-{
-    uint8_t request = make_packet_request(ADIV5_LOW_READ, addr);
-    xsm->waitTxEmpty();
-    zwrite(8, request);
-    uint32_t ack = zread(3);
-    uint32_t data = zread(32);
-    bool parity = zread(1);
-    zwrite(8, 0);
-    return ack == SWD_ACK_OK ? data : 0;
-}
-/**
-        \fn ln_adiv5_swd_raw_access
-
- */
-extern "C" uint32_t ln_adiv5_swd_raw_access(adiv5_debug_port_s *dp, const uint8_t rnw, const uint16_t addr,
-                                            const uint32_t value)
-{
-    //
-    if ((addr & ADIV5_APnDP) && dp->fault)
-        return 0;
-
-    const uint8_t request = make_packet_request(rnw, addr);
-    uint8_t ack = SWD_ACK_WAIT;
-    platform_timeout_s timeout;
-    platform_timeout_set(&timeout, 250U);
-    while (1)
-    {
-
-        xsm->waitTxEmpty();
-        zwrite(8, request);
-        uint32_t ack = zread(3);
-        bool expired = platform_timeout_is_expired(&timeout);
-        if (ack == SWD_ACK_OK)
-        {
-            goto done;
-        }
-        // if we get here, we are retrying
-        // switch back to output
-        zwrite(2, 0x03);
-        switch (ack)
-        {
-        case SWD_ACK_OK:
-            xAssert(0);
-            break;
-        case SWD_ACK_NO_RESPONSE:
-            DEBUG_ERROR("SWD access resulted in no response\n");
-            dp->fault = ack;
-            return 0;
-
-        case SWD_ACK_WAIT:
-            if (expired)
-            {
-                DEBUG_ERROR("SWD access resulted in wait, aborting\n");
-                if (dp->fault == SWD_ACK_WAIT)
-                    return 0;
-                dp->fault = ack; // prevent recursion
-                dp->abort(dp, ADIV5_DP_ABORT_DAPABORT);
-                dp->fault = ack;
-                return 0;
-            }
-            break;
-        case SWD_ACK_FAULT:
-            if (expired)
-            {
-                DEBUG_ERROR("SWD access resulted in fault\n");
-                dp->fault = ack;
-                return 0;
-            }
-            DEBUG_ERROR("SWD access resulted in fault, retrying\n");
-            /* On fault, abort the request and repeat */
-            /* Yes, this is self-recursive.. no, we can't think of a better option */
-            adiv5_dp_write(dp, ADIV5_DP_ABORT,
-                           ADIV5_DP_ABORT_ORUNERRCLR | ADIV5_DP_ABORT_WDERRCLR | ADIV5_DP_ABORT_STKERRCLR |
-                               ADIV5_DP_ABORT_STKCMPCLR);
-            break;
-        default:
-            DEBUG_ERROR("SWD access has invalid ack %x\n", ack);
-            raise_exception(EXCEPTION_ERROR, "SWD invalid ACK");
-            break;
-        }
+#define SWINDLE_FAST_IO
+#define SWD_WAIT_PERIOD()                                                                                              \
+    {                                                                                                                  \
     }
-done:
-    // We are out of read sequence, CLK is low
-    if (rnw) // read ****************************************** HERE *************************
-    {
-        uint32_t index = 1;
-        uint32_t response = zread(32);
-        bool parityBit = zread(3) & 1;
-        bool currentParity = lnOddParity(response);
-        zwrite(8, 0);
-        if (currentParity != parityBit)
-        { /* Give up on parity error */
-            dp->fault = 1U;
-            DEBUG_ERROR("SWD access resulted in parity error\n");
-            raise_exception(EXCEPTION_ERROR, "SWD parity error");
-        }
-        return response;
+#define DIR_INPUT()                                                                                                    \
+    {                                                                                                                  \
     }
-    // write
-    bool parity = lnOddParity(value);
-    zwrite(2, 0x03);
-    zwrite(32, value);
-    xsm->waitTxEmpty();
-    zwrite(1, parity);
-    zwrite(8, 0);
-    return 0;
-    //--
-}
+#define DIR_OUTPUT()                                                                                                   \
+    {                                                                                                                  \
+    }
 
-/**
- */
-extern "C" void ln_raw_swd_write(uint32_t tick, uint32_t value)
-{
-    zwrite(tick, value);
-}
-// EOF
+#include "../swd_template.h"
