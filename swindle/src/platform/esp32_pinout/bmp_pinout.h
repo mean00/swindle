713f479ea357ebaad3387c43f33e3579687cde4a
mean <fixounet@free.fr>
2026-03-17 19:53:38 +0100

    esp: split board pinout between normal and mini

--- swindle/src/platform/esp32_pinout/bmp_pinout.h
+++ swindle/src/platform/esp32_pinout/bmp_pinout.h
@@ -1,22 +1,13 @@
 #pragma once
 #include "lnGPIO_pins.h"
-// mapping of BMP gpio to the GPIO we use
-const lnPin _mapping[9] = {
-    GPIO0, // 0 TMS_PIN
-    GPIO0, // 1 TDI_PIN
-    GPIO0, // 2 TDO_PIN
-    GPIO0, // 3 TCK_PIN
-    GPIO0, // 4 TRACESWO_PIN
 
-    GPIO7, // 5 SWDIO_PIN
-    GPIO8, // 6 SWCLK_PIN
+#ifdef LN_ESP_MINI
+#include "mini/bmp_pinout.h"
+#else
+#include "wroom/bmp_pinout.h"
+#endif
 
-    GPIO9,  // 7 RST
-    GPIO11, // 8 direction
-};
-
-#define PIN_ADC_NRESET_DIV_BY_TWO PA3 // this pins is connected to NRST/2
-#define PIN_ADC_NRESET_MULTIPLIER 1.  // 2.0 if divided by 2 , 1.0 if not divided
+#define PIN_ADC_NRESET_MULTIPLIER 1. // 2.0 if divided by 2 , 1.0 if not divided
 
 #define LN_USB_INSTANCE 1
 #define LN_SERIAL_INSTANCE 2
